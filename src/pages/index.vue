199d822a4b649abf73a554173003b7a5372c3c23
Anderson Augusto Rodrigo Silva <andersonaugustorodrigosilva@MacBook-Pro-de-Anderson.local>
2018-04-11 22:40:55 -0300

    Primeiro comit

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+     
+      <q-btn label="Nova Noticia" @click="abreNovaNoticia()"/>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    abreNovaNoticia () {
+      this.$router.push({ path: '/noticias' })
+    }
+  }
+}
+</script>
---
+++ src/pages/noticias.vue
@@ -0,0 +1,55 @@
+<template>
+  <q-page padding>
+    <q-input v-model="titulo" float-label="Titulo"/>
+    <q-datetime v-model="data" type="date" float-label="Data" format='DD/MM/YYYY'/>
+    <!--q-input v-model="conteudo" float-label="Conteudo"
+     type="textarea"
+    :max-height="100"
+    rows="7"/-->
+
+
+      <vue-editor v-model="conteudo"></vue-editor>
+    
+    <q-input v-model="tags" float-label="Tags"/>
+
+      <q-btn label="Cancelar" @click="salvar()"/>
+      <q-btn label="Salvar" @click="salvar()"/>
+  </q-page>
+</template>
+
+<script>
+import Ckeditor from 'vue-ckeditor2'
+ import { VueEditor } from 'vue2-editor'
+export default {
+   components: { Ckeditor, VueEditor},
+   name: 'Noticias',
+   data () {
+    return {
+      titulo: '',
+      data: new Date(),
+      conteudo: '',
+      tags: '',
+      config: {
+        toolbar: [
+          [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript' ]
+        ],
+        height: 300
+      }
+    }
+  },
+  methods: {
+    salvar () {
+      this.$router.push({ path: '/' })
+    },
+    onBlur (editor) {
+      console.log(editor)
+    },
+    onFocus (editor) {
+      console.log(editor)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
